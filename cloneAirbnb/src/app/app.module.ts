4ee8f8c7e828cbaf2937647ce137895d28006103
Woochul Hyun <woochul.hyun91@gmail.com>
2019-07-13 18:20:05 +0900

    shared module

--- cloneAirbnb/src/app/app.module.ts
+++ cloneAirbnb/src/app/app.module.ts
@@ -2,19 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- cloneAirbnb/src/app/shared/shared.module.ts
+++ cloneAirbnb/src/app/shared/shared.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
-  declarations: [NavigationComponent],
-  imports: [
-    CommonModule,
-    SharedRoutingModule
-  ]
+  declarations: [NavigationComponent, NotFoundComponent],
+  imports: [CommonModule, SharedRoutingModule],
+  exports: [NavigationComponent, NotFoundComponent]
 })
-export class SharedModule { }
+export class SharedModule {}
