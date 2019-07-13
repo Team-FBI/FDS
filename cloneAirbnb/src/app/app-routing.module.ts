502f9ef30e588d5612be4e24f939f822c1c82413
Woochul Hyun <woochul.hyun91@gmail.com>
2019-07-14 00:59:55 +0900

    not found page

--- cloneAirbnb/src/app/app-routing.module.ts
+++ cloneAirbnb/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
