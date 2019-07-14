6a44b0129ad974b8f2be916f280ff8817e10565b
Woochul Hyun <woochul.hyun91@gmail.com>
2019-07-14 19:16:08 +0900

    use redirecteTo for the first page

--- cloneAirbnb/src/app/app-routing.module.ts
+++ cloneAirbnb/src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
   // { path: '**', component: NotFoundComponent }
 ];
 
--- cloneAirbnb/src/app/pages/pages-routing.module.ts
+++ cloneAirbnb/src/app/pages/pages-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
 import { StorageListComponent } from './storage-list/storage-list.component';
 import { YourTripComponent } from './your-trip/your-trip.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: 'storageList', component: StorageListComponent },
   { path: 'yourTrip', component: YourTripComponent }
 ];
