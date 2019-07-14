16be292aff07d0032502394ed0e2477fcc491c6b
Woochul Hyun <woochul.hyun91@gmail.com>
2019-07-15 03:36:25 +0900

    navigation and font awesome

--- cloneAirbnb/src/app/app.module.ts
+++ cloneAirbnb/src/app/app.module.ts
@@ -5,12 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { SharedModule } from './shared/shared.module';
 import { PagesModule } from './pages/pages.module';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, PagesModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    PagesModule,
+    AngularFontAwesomeModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
