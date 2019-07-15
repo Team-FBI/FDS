9299f19f34d39dea475cf3cb9fcde4f85f0fa7b0
Woochul Hyun <woochul.hyun91@gmail.com>
2019-07-15 13:38:13 +0900

    favicon and bug fix

--- cloneAirbnb/src/app/shared/shared.module.ts
+++ cloneAirbnb/src/app/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { RouterModule } from '@angular/router';
 import { NavigationComponent } from './components/navigation/navigation.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [NavigationComponent, NotFoundComponent],
