bf5a5efe1da2c1b6112ac2baf2afe00b5fd86d7b
Woochul Hyun <woochul.hyun91@gmail.com>
2019-07-15 13:39:14 +0900

    delete oninit

--- cloneAirbnb/src/app/shared/not-found/not-found.component.ts
+++ cloneAirbnb/src/app/shared/not-found/not-found.component.ts
@@ -5,11 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss']
 })
-export class NotFoundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class NotFoundComponent {
+  constructor() {}
 }
