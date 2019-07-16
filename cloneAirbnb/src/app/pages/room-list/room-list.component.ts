b256ca4ce4bb7690c42fd40b55daf19d644ea8f5
sunnykim91 <tjsgnl2002@gmail.com>
2019-07-16 21:51:24 +0900

    add infowindow maybe need update

--- cloneAirbnb/src/app/pages/room-list/room-list.component.ts
+++ cloneAirbnb/src/app/pages/room-list/room-list.component.ts
@@ -11,17 +11,48 @@ export class RoomListComponent  {
   longitude = 126.52811723292518;
   selectedMarker;
   markers = [
-    { lat: 33.36995865711402, lng: 126.52811723292518, alpha: 1 },
-    { lat: 33.36995865711402, lng: 126.54811723292518, alpha: 1 }
+    { lat: 33.36995865711402, lng: 126.52811723292518, alpha: 1 , content: 'asdf'},
+    { lat: 33.36995865711402, lng: 126.54811723292518, alpha: 1 , content: 'ehll'}
   ];
   zoomControlOptions: ZoomControlOptions = {
     position: ControlPosition.LEFT_TOP,
     style: ZoomControlStyle.LARGE
   };
-  // imgsrc = [
-  //   'https://a0.muscache.com/im/pictures/8cca891c-d4c2-45f0-9623-0c7c0f46fccf.jpg?aki_policy=large',
-  //   'https://a0.muscache.com/im/pictures/89587324/26c55a69_original.jpg?aki_policy=large'
-  // ]
+
+  imgsrc = [
+    'https://a0.muscache.com/im/pictures/8cca891c-d4c2-45f0-9623-0c7c0f46fccf.jpg?aki_policy=large',
+    'https://a0.muscache.com/im/pictures/89587324/26c55a69_original.jpg?aki_policy=large'
+  ]
+
+  contents = [
+    {
+      content: `<div class="infowindow">
+              <div>
+               <img class="infoimg" ${this.imgsrc[1]}
+               src="https://a0.muscache.com/im/pictures/89587324/26c55a69_original.jpg?aki_policy=large">
+              </div>
+              <div>
+                <p>집 전체·침대1개</p>
+                <p>제주도를 가로지르는 아름다운 1100로위 "jeju901"</p>
+              </div>
+            </div>`
+      },
+      {
+      content: `<div class="infowindow">
+              <div>
+               <img class="infoimg" ${this.imgsrc[1]}
+               src="https://a0.muscache.com/im/pictures/89587324/26c55a69_original.jpg?aki_policy=large">
+              </div>
+              <div>
+                <p>집2333331개</p>
+                <p>제주도를 가로지르는 아름다운 1100로위 "jeju901"</p>
+              </div>
+            </div>`
+    }
+
+  ]
+
+ 
   
   addMarker(lat: number, lng: number) {
     this.markers.push({ lat, lng, alpha: 0.4 });
