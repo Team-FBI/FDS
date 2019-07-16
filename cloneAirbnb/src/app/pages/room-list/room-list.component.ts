cd82e7a3d53f70ca6992ec6c1d4e60d33a507e09
sunnykim91 <tjsgnl2002@gmail.com>
2019-07-16 16:07:15 +0900

    test

--- cloneAirbnb/src/app/pages/room-list/room-list.component.ts
+++ cloneAirbnb/src/app/pages/room-list/room-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Options } from 'selenium-webdriver/edge';
 
 @Component({
   selector: 'app-room-list',
@@ -11,14 +12,11 @@ export class RoomListComponent  {
   selectedMarker;
   markers = [
     // These are all just random coordinates from https://www.random.org/geographic-coordinates/
-    { lat: 33.36995865711402, lng: 126.52811723292518, alpha: 1 },
-    { lat: 7.92658, lng: -12.05228, alpha: 1 },
-    { lat: 48.75606, lng: -118.859, alpha: 1 },
-    { lat: 5.19334, lng: -67.03352, alpha: 1 },
-    { lat: 12.09407, lng: 26.31618, alpha: 1 },
-    { lat: 47.92393, lng: 78.58339, alpha: 1 }
+    { lat: 33.36995865711402, lng: 126.52811723292518, alpha: 1 }
+    
   ];
 
+  
   addMarker(lat: number, lng: number) {
     this.markers.push({ lat, lng, alpha: 0.4 });
   }
@@ -36,5 +34,9 @@ export class RoomListComponent  {
       lat: event.latitude,
       lng: event.longitude
     };
+  }
+
+  markerClose(){
+    // console.log();
   }
 }
