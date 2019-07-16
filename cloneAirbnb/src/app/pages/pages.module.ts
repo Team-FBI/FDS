67b8c576f3a195423e1b1dd908fa27435997fdf1
midalee <midahaha@naver.com>
2019-07-16 16:47:34 +0900

    add search form in home component

--- cloneAirbnb/src/app/pages/pages.module.ts
+++ cloneAirbnb/src/app/pages/pages.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 import { HomeComponent } from './home/home.component';
 import { StorageListComponent } from './storage-list/storage-list.component';
@@ -10,6 +12,6 @@ import { YourTripComponent } from './your-trip/your-trip.component';
 
 @NgModule({
   declarations: [HomeComponent, StorageListComponent, YourTripComponent],
-  imports: [CommonModule, PagesRoutingModule, SharedModule]
+  imports: [CommonModule, PagesRoutingModule, SharedModule, BrowserAnimationsModule,  BsDatepickerModule.forRoot()]
 })
 export class PagesModule {}
