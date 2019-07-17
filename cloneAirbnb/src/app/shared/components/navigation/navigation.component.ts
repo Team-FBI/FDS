98c88db3d1440585af16f1026f359edd56b211c6
midalee <midahaha@naver.com>
2019-07-17 13:52:29 +0900

    home

--- cloneAirbnb/src/app/shared/components/navigation/navigation.component.ts
+++ cloneAirbnb/src/app/shared/components/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+
+  constructor( private router: Router) {}
+
+  ngOnInit() {
+
+  }
+
 }
