f7061c6df3b44b12dcc581dae2914291921b1af4
hbs <hbsowo58@gmail.com>
2019-07-18 20:48:37 +0900

    edit carusel

--- cloneAirbnb/src/app/pages/room-detail/room-detail.component.ts
+++ cloneAirbnb/src/app/pages/room-detail/room-detail.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild, AfterViewInit } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
+import { UrlRememberService } from 'src/app/core/service/url-remember.service';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 
 @Component({
   selector: 'app-room-detail',
   templateUrl: './room-detail.component.html',
   styleUrls: ['./room-detail.component.scss']
 })
-export class RoomDetailComponent implements OnInit {
+export class RoomDetailComponent implements OnInit, AfterViewInit {
   minDate: Date;
   maxDate: Date;
   modalRef: BsModalRef;
@@ -18,17 +21,43 @@ export class RoomDetailComponent implements OnInit {
   Youngcounter = 0;
   appUrl: string = environment.appUrl;
 
+  @ViewChild('galleryTop', { static: true }) galleryTop;
+  @ViewChild('galleryThumbs', { static: true }) galleryThumbs;
+  isOpen = false;
+
+  galleryTopConfig: SwiperConfigInterface = {
+    spaceBetween: 10,
+    effect: 'fade',
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  };
+  galleryThumbsConfig: SwiperConfigInterface = {
+    spaceBetween: 10,
+    slidesPerView: 9,
+    centeredSlides: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    slideToClickedSlide: true,
+  };
+
   config = {
     ignoreBackdropClick: true
   };
 
-  constructor(private modalService: BsModalService, private http: HttpClient, ) {
+  constructor(
+    private modalService: BsModalService,
+    private http: HttpClient,
+    private router: Router,
+    private urlRemember: UrlRememberService
+    ) 
+    {
     this.minDate = new Date();
     this.maxDate = new Date();
     this.minDate.setDate(this.minDate.getDate() - 1);
     this.maxDate.setDate(this.maxDate.getDate() + 7);
-    
-  }
+    }
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.modalService.show(template);
   }
@@ -39,7 +68,9 @@ export class RoomDetailComponent implements OnInit {
   
 
   ngOnInit() {
-    this.http.get(`${this.appUrl}/rooms/1`)
+    this.urlRemember.currentUrl = this.router.url;
+
+    this.http.get(`${this.appUrl}/rooms/`)
       .subscribe(res => console.log(res))
 
   }
@@ -67,5 +98,10 @@ export class RoomDetailComponent implements OnInit {
       this.Youngcounter--;
     }
     this.Allcounter = this.Adultcounter + this.Childcounter + this.Youngcounter;
+  }
+
+  ngAfterViewInit() {
+    this.galleryTop.nativeElement.swiper.controller.control = this.galleryThumbs.nativeElement.swiper;
+    this.galleryThumbs.nativeElement.swiper.controller.control = this.galleryTop.nativeElement.swiper;
   }
 }
