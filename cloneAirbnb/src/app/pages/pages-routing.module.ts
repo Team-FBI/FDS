ef9ae12742648eec738fa0dc419c179418eca1be
Woochul Hyun <woochul.hyun91@gmail.com>
2019-07-19 18:49:38 +0900

    merge

--- cloneAirbnb/src/app/pages/pages-routing.module.ts
+++ cloneAirbnb/src/app/pages/pages-routing.module.ts
@@ -5,11 +5,11 @@ import { HomeComponent } from './home/home.component';
 import { StorageListComponent } from './storage-list/storage-list.component';
 import { YourTripComponent } from './your-trip/your-trip.component';
 import { RoomListComponent } from './room-list/room-list.component';
-import { RoomRegulationComponent } from './room-regulation/room-regulation.component';
-import { PaymentComponent } from './payment/payment.component';
-import { GuestInfoComponent } from './guest-info/guest-info.component';
-import { RoomdetailInfoComponent } from './roomdetail-info/roomdetail-info.component';
 import { RoomDetailComponent } from './room-detail/room-detail.component';
+import { RoomRegulationComponent } from './room-regulation/room-regulation.component';
+import { GuestInfoComponent } from './guest-info/guest-info.component';
+import { PaymentComponent } from './payment/payment.component';
+
 import { AuthGuard } from '../core/guard/auth.guard';
 
 const routes: Routes = [
@@ -20,14 +20,22 @@ const routes: Routes = [
     canActivate: [AuthGuard]
   },
   { path: 'yourTrip', component: YourTripComponent, canActivate: [AuthGuard] },
+  { path: 'roomList', component: RoomListComponent },
   {
     path: 'roomdetail',
     component: RoomDetailComponent
   },
-  { path: 'roomregulation', component: RoomRegulationComponent },
-  { path: 'payment', component: PaymentComponent },
-  { path: 'guestinfo', component: GuestInfoComponent },
-  { path: 'roomdetailinfo', component: RoomdetailInfoComponent }
+  {
+    path: 'roomregulation',
+    component: RoomRegulationComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'guestinfo',
+    component: GuestInfoComponent,
+    canActivate: [AuthGuard]
+  },
+  { path: 'payment', component: PaymentComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
