f82bc2c098cced399748598793c57fba6595adde
hbs <hbsowo58@gmail.com>
2019-07-24 15:13:27 +0900

    edit detailcontent

--- cloneAirbnb/src/app/pages/roomdetail-info/roomdetail-info.component.ts
+++ cloneAirbnb/src/app/pages/roomdetail-info/roomdetail-info.component.ts
@@ -30,7 +30,7 @@ export class RoomdetailInfoComponent implements OnInit {
   ngOnInit() {
     this.urlRemember.currentUrl = this.router.url;
 
-    this.http.get(`${this.appUrl}/rooms/2`)
+    this.http.get(`${this.appUrl}/rooms/5/`)
       .subscribe( (res: any) => { 
       this.title = res.title;
       this.address = res.address;
