495015bcc84bc01cffa2e919eb71b1ae1b2cc543
Woochul Hyun <woochul.hyun91@gmail.com>
2019-07-28 22:02:53 +0900

    support multiple language for all components

--- cloneAirbnb/src/app/pages/check-payment/check-payment.component.ts
+++ cloneAirbnb/src/app/pages/check-payment/check-payment.component.ts
@@ -3,25 +3,34 @@ import { faCreditCard } from '@fortawesome/free-solid-svg-icons/faCreditCard';
 import { faCcMastercard } from '@fortawesome/free-brands-svg-icons/faCcMastercard';
 import { faCcVisa } from '@fortawesome/free-brands-svg-icons/faCcVisa';
 import { faCcAmex } from '@fortawesome/free-brands-svg-icons/faCcAmex';
-
+import { TranslateService } from '@ngx-translate/core';
+import { LanguageService } from 'src/app/core/service/language.service';
 
 @Component({
   selector: 'app-check-payment',
   templateUrl: './check-payment.component.html',
   styleUrls: ['./check-payment.component.scss']
 })
-export class CheckPaymentComponent   {
+export class CheckPaymentComponent implements OnInit {
   faCreditCard = faCreditCard;
   faCcMastercard = faCcMastercard;
   faCcVisa = faCcVisa;
   faCcAmex = faCcAmex;
   toggleCard = false;
 
-  addCard(){
+  constructor(
+    private translate: TranslateService,
+    private languageService: LanguageService
+  ) {}
+
+  ngOnInit() {
+    this.translate.setDefaultLang(`${this.languageService.currentLanguage()}`);
+  }
+
+  addCard() {
     if (this.toggleCard) {
       this.toggleCard = false;
-    }
-    else {
+    } else {
       this.toggleCard = true;
     }
   }
--- cloneAirbnb/src/app/pages/guest-info/guest-info.component.ts
+++ cloneAirbnb/src/app/pages/guest-info/guest-info.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
 import { Router } from '@angular/router';
+
 import { UrlRememberService } from 'src/app/core/service/url-remember.service';
+import { LanguageService } from 'src/app/core/service/language.service';
 
 @Component({
   selector: 'app-guest-info',
@@ -8,14 +12,15 @@ import { UrlRememberService } from 'src/app/core/service/url-remember.service';
   styleUrls: ['./guest-info.component.scss']
 })
 export class GuestInfoComponent implements OnInit {
-
   constructor(
     private router: Router,
-    private urlRemember: UrlRememberService
-  ) { }
+    private urlRemember: UrlRememberService,
+    private translate: TranslateService,
+    private languageService: LanguageService
+  ) {}
 
   ngOnInit() {
     this.urlRemember.currentUrl = this.router.url;
+    this.translate.setDefaultLang(`${this.languageService.currentLanguage()}`);
   }
-
 }
