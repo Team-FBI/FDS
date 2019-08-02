41802443bab367479a1874f2ed073ae9e0c6594a
Woochul Hyun <woochul.hyun91@gmail.com>
2019-08-02 19:55:44 +0900

    message feat

--- cloneAirbnb/src/app/pages/message/message.component.ts
+++ cloneAirbnb/src/app/pages/message/message.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { WebsocketService } from 'src/app/core/service/websocket.service';
+import { ChatService } from 'src/app/core/service/chat.service';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
-  styleUrls: ['./message.component.scss']
+  styleUrls: ['./message.component.scss'],
+  providers: [WebsocketService, ChatService]
 })
 export class MessageComponent implements OnInit {
+  appUrl: string = environment.appUrl;
+  messages = [];
+  messageHistory = [];
 
-  constructor() { }
+  message = {
+    message: ''
+  };
 
-  ngOnInit() {
+  constructor(private chatService: ChatService, private http: HttpClient) {
+    chatService.messages.subscribe(msg => {
+      console.log('Response from websocket: ' + msg);
+      console.log(msg);
+      this.messages.push(msg);
+    });
   }
 
+  ngOnInit() {
+    this.http.get(`${this.appUrl}/chat/44/`).subscribe((res: any) => {
+      console.log(res);
+      for (const prvMessage of res.messages) {
+        this.messageHistory.push(prvMessage);
+      }
+      console.log(this.messageHistory);
+    });
+  }
+
+  sendMsg(userInput: HTMLInputElement) {
+    this.message.message = userInput.value;
+    this.chatService.messages.next(this.message);
+    userInput.value = '';
+  }
 }
